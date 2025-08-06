5b63046dd3dd3a35ed930409630705be84b7454b
MarlusCSilva <marlussilva5816@gmail.com>
2025-08-05 21:59:42 -0300

    CadFuncionario - estruturando

---
+++ src/SistemaDeBiblioteca/CadFuncionario.java
@@ -0,0 +1,18 @@
+package SistemaDeBiblioteca;
+import java.util.ArrayList;
+
+public class CadFuncionario {
+    private ArrayList<Funcionario> funcionarios;
+
+    public CadFuncionario() {
+        this.funcionarios = new ArrayList<>();
+    }
+
+    public ArrayList<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(ArrayList<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+}
