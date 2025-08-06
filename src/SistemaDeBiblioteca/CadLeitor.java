5f861e5847c526591280d313d657e006f21de8df
MarlusCSilva <marlussilva5816@gmail.com>
2025-08-05 21:51:45 -0300

    CadLeitor - estrurando

--- src/SistemaDeBiblioteca/CadLeitor.java
+++ src/SistemaDeBiblioteca/CadLeitor.java
@@ -5,4 +5,14 @@ import java.util.List;
 public class CadLeitor {
     private List<Leitor> leitores;
 
+    public CadLeitor() {
+        this.leitores = new ArrayList<>();
+    }
+    public ArrayList<Leitor> getLeitores() {
+        return leitores;
+    }
+    public void setLeitores(ArrayList<Leitor> leitores) {
+        this.leitores = leitores;
+    }
+
 }
